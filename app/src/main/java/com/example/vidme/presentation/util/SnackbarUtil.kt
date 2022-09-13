3ee086968e433b26918ebe66f98d9fb9ab620538
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-09-13 20:27:39 +0200

    add icons to snackBars

--- app/src/main/java/com/example/vidme/presentation/util/SnackbarUtil.kt
+++ app/src/main/java/com/example/vidme/presentation/util/SnackbarUtil.kt
@@ -1,7 +1,13 @@
 package com.example.vidme.presentation.util
 
 import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
+import com.example.vidme.R
 import com.google.android.material.snackbar.Snackbar
 
 private const val animationMode = Snackbar.ANIMATION_MODE_SLIDE
@@ -15,18 +21,34 @@ private fun getColor(color: String): Int {
 }
 
 fun showWarningSnackBar(view: View, message: String) {
+    val icon = ContextCompat.getDrawable(view.context, R.drawable.ic_warning)!!
     snackBarBuilder(view, message).setBackgroundTint(getColor("#ECF009"))
         .setTextColor(getColor("#141313"))
+        .setIcon(icon, getColor("#101010"))
         .show()
 }
 
 fun showErrorSnackBar(view: View, message: String) {
+    val icon = ContextCompat.getDrawable(view.context, R.drawable.ic_error_outline)!!
     snackBarBuilder(view, message)
         .setBackgroundTint(getColor("#A50404"))
         .setTextColor(getColor("#E3E3E3"))
+        .setIcon(icon, getColor("#E3E3E3"))
         .show()
 }
 
 fun showSimpleSnackBar(view: View, message: String) {
     snackBarBuilder(view, message).show()
+}
+
+private fun Snackbar.setIcon(drawable: Drawable, @ColorInt colorTint: Int): Snackbar {
+    return this.apply {
+        setAction(" ") {}
+        val textView = view.findViewById<TextView>(com.google.android.material.R.id.snackbar_action)
+        textView.text = ""
+
+        drawable.setTint(colorTint)
+        drawable.setTintMode(PorterDuff.Mode.SRC_ATOP)
+        textView.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+    }
 }
