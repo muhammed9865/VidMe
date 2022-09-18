87d2a2b0829f20f2c6e6c6f16c2ede2acb4a832c
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-09-18 02:20:14 +0200

    add downloadInfo to VideoInfo.kt if it's currently being downloaded

--- app/src/main/java/com/example/vidme/domain/pojo/VideoInfo.kt
+++ app/src/main/java/com/example/vidme/domain/pojo/VideoInfo.kt
@@ -4,10 +4,12 @@ data class VideoInfo(
     val id: String,
     val title: String,
     val url: String,
+    val thumbnail: String,
     val duration: String,
     val isAudio: Boolean,
     val isVideo: Boolean,
     val isDownloaded: Boolean,
     val playlistName: String?,
     val isDownloading: Boolean = false,
+    val downloadInfo: DownloadInfo? = null,
 )
--- app/src/main/java/com/example/vidme/domain/pojo/YoutubePlaylistWithVideos.kt
+++ app/src/main/java/com/example/vidme/domain/pojo/YoutubePlaylistWithVideos.kt
@@ -1,6 +1,6 @@
 package com.example.vidme.domain.pojo
 
-import com.example.vidme.domain.util.StringUtil
+import com.example.vidme.domain.util.StringUtil.calculateDuration
 import com.example.vidme.domain.util.StringUtil.durationAsString
 
 data class YoutubePlaylistWithVideos(
@@ -12,30 +12,6 @@ data class YoutubePlaylistWithVideos(
     fun getOriginalUrl() = playlistInfo.originalUrl
     fun getFullDuration() = durationAsString(videos.sumOf { calculateDuration(it.duration) })
 
-    private fun calculateDuration(duration: String): Int {
-        return if (StringUtil.isDigitsOnly(duration)) {
-            duration.toInt()
-        } else {
-            var durationAsInt = 0
-            val timeSplit = duration.split(":").map { it.toInt() }
-            when (timeSplit.size) {
-                3 -> {
-                    durationAsInt += timeSplit[0] * 3600
-                    durationAsInt += timeSplit[1] * 60
-                    durationAsInt += timeSplit[2]
-                }
-                2 -> {
-                    durationAsInt += timeSplit[0] * 60
-                    durationAsInt += timeSplit[1]
-                }
-                1 -> {
-                    durationAsInt += timeSplit[0]
-                }
-            }
-
-            durationAsInt
-        }
-    }
 
 
 }
