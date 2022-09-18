cdc044fcad1c3dd310b9898dc62316deeea9b835
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-09-18 02:19:03 +0200

    added SingleDownloadListener that's used as interface between mainViewModel and SingleViewHolder during download

--- app/src/main/java/com/example/vidme/presentation/viewholder/ItemsListListeners.kt
+++ app/src/main/java/com/example/vidme/presentation/viewholder/ItemsListListeners.kt
@@ -2,8 +2,10 @@ package com.example.vidme.presentation.viewholder
 
 import com.example.vidme.domain.pojo.VideoInfo
 import com.example.vidme.domain.pojo.YoutubePlaylistInfo
+import com.example.vidme.presentation.callback.SingleDownloadState
 
 
 typealias PlaylistInfoListener = (playlist: YoutubePlaylistInfo) -> Unit
 
 typealias SingleListener = (single: VideoInfo) -> Unit
+typealias SingleDownloadListener = (single: VideoInfo, listener: SingleDownloadState) -> Unit
