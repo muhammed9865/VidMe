dbcafbe5d0f61c567c55f5fed53f88b50252b950
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-10-03 13:16:57 +0200

    fix containsDuration returns false if duration is seconds < 10

--- app/src/main/java/com/example/vidme/domain/util/StringUtil.kt
+++ app/src/main/java/com/example/vidme/domain/util/StringUtil.kt
@@ -2,27 +2,36 @@ package com.example.vidme.domain.util
 
 object StringUtil {
     fun containsDuration(line: String): Boolean {
-        return line.matches(Regex("(?:(\\d{1,3}):)?(\\d{1,2}):(\\d{2})")) || (isDigitsOnly(line) && line.length == 2)
+        return line.matches(Regex("(?:(\\d{1,3}):)?(\\d{1,2}):(\\d{2})")) || (isDigitsOnly(line) && line.isNotEmpty())
     }
 
     fun isDigitsOnly(line: String): Boolean {
         return line.all { it.isDigit() }
     }
 
-    fun durationAsString(duration: Int): String {
-        val hours = duration / 3600
-        val minutes = (duration - 3600 * hours) / 60
-        val seconds = (duration - 3600 * hours - 60 * minutes)
-        val unit = if (hours > 0) "h" else if (minutes > 0) "m" else "s"
-        return buildString {
-            if (hours > 0) append("$hours:")
-            append("$minutes:")
-            if (seconds < 10) {
-                append("0$seconds")
-            } else
-                append(seconds)
-            append(unit)
+    fun durationAsString(duration: Int, appendUnit: Boolean = true): String {
+        try {
+            val hours = duration / 3600
+            val minutes = (duration - 3600 * hours) / 60
+            val seconds = (duration - 3600 * hours - 60 * minutes)
+            val unit = if (hours > 0) "h" else if (minutes > 0) "m" else "s"
+            return buildString {
+                if (hours > 0) append("$hours:")
+                append("$minutes:")
+                if (seconds < 10) {
+                    append("0$seconds")
+                } else
+                    append(seconds)
+                if (appendUnit)
+                    append(unit)
+            }
+        } catch (e: NumberFormatException) {
+            return "0:00"
         }
+    }
+
+    fun durationFromMilli(millis: Int): String {
+        return durationAsString(millis / 1000, appendUnit = false)
     }
 
     fun calculateDuration(duration: String): Int {
