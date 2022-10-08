3f74c2b8730e1ae63629683dbc601542a40cd422
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-10-08 21:30:34 +0200

    fix: change storage from external to internal due to permissions restriction

--- app/src/main/java/com/example/vidme/domain/util/FileUtil.kt
+++ app/src/main/java/com/example/vidme/domain/util/FileUtil.kt
@@ -2,63 +2,68 @@ package com.example.vidme.domain.util
 
 import android.net.Uri
 import android.os.Environment
+import com.example.vidme.VidApplication
 import kotlinx.coroutines.coroutineScope
 import timber.log.Timber
 import java.io.File
 
-object FileUtil {
-    private fun getStorageFile(): File =
-        File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-            "VidMe").apply {
-            if (!exists()) {
-                mkdir()
+class FileUtil {
+    companion object {
+
+        private fun getStorageFile(): File = kotlin.run {
+            val context = VidApplication.instance.applicationContext
+            File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+                "VidMe").apply {
+                if (!exists()) {
+                    mkdir()
+                }
             }
         }
 
-    fun getStorageUri(): Uri = Uri.parse(getStorageFile().absolutePath)
+        fun getStorageUri(): Uri = Uri.parse(getStorageFile().absolutePath)
 
-    fun getStorageFileForPlaylist(playlistName: String): File {
-        return File(getStorageFile(), playlistName).apply {
-            if (!exists()) {
-                mkdir()
+        fun getStorageFileForPlaylist(playlistName: String): File {
+            return File(getStorageFile(), playlistName).apply {
+                if (!exists()) {
+                    mkdir()
+                }
+            }
+        }
+
+        suspend fun deletePlaylistByName(playlistName: String): Boolean {
+            return coroutineScope {
+                try {
+                    val file = getStorageFileForPlaylist(playlistName)
+                    file.deleteRecursively()
+
+                } catch (e: Exception) {
+                    false
+                }
+            }
+        }
+
+        fun checkIfMediaExists(name: String): Pair<String?, Boolean> {
+            val storedFiles = getStorageFile().listFiles()
+            storedFiles?.forEach { file ->
+                if (file.nameWithoutExtension == name) {
+                    Timber.d("${file.nameWithoutExtension} : $name")
+                    return file.absolutePath to true
+                }
+            }
+            return null to false
+
+        }
+
+        suspend fun deleteVideoByName(location: String?): Boolean {
+            return coroutineScope {
+                try {
+                    val file = location?.let { File(it) }
+                    file?.delete() ?: false
+                } catch (e: Exception) {
+                    Timber.d(e)
+                    false
+                }
             }
         }
     }
-
-    suspend fun deletePlaylistByName(playlistName: String): Boolean {
-        return coroutineScope {
-            try {
-                val file = getStorageFileForPlaylist(playlistName)
-                file.deleteRecursively()
-
-            } catch (e: Exception) {
-                false
-            }
-        }
-    }
-
-    fun checkIfMediaExists(name: String): Pair<String?, Boolean> {
-        val storedFiles = getStorageFile().listFiles()
-        storedFiles?.forEach { file ->
-            if (file.nameWithoutExtension == name) {
-                Timber.d("${file.nameWithoutExtension} : $name")
-                return file.absolutePath to true
-            }
-        }
-        return null to false
-
-    }
-
-    suspend fun deleteVideoByName(location: String?): Boolean {
-        return coroutineScope {
-            try {
-                val file = location?.let { File(it) }
-                file?.delete() ?: false
-            } catch (e: Exception) {
-                Timber.d(e)
-                false
-            }
-        }
-    }
-
 }
