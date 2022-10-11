de4a2656fb210d92bc83211e37efeda94916f90d
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-10-11 21:25:57 +0200

    edit: check if playlist singles are not all downloaded before starting a new download.

--- app/src/main/java/com/example/vidme/domain/usecase/DownloadYoutubePlaylistUseCase.kt
+++ app/src/main/java/com/example/vidme/domain/usecase/DownloadYoutubePlaylistUseCase.kt
@@ -7,11 +7,26 @@ import javax.inject.Inject
 
 class DownloadYoutubePlaylistUseCase @Inject constructor() : BaseUseCase() {
 
+    /*
+        Checking first if playlist videos are all downloaded
+        if not all downloaded, proceed to data layer and downloadPlaylist
+        else, send DataState that states download is finished.
+     */
     suspend operator fun invoke(
         playlistRequest: PlaylistRequest,
         onDownloadInfo: (DataState<DownloadInfo>) -> Unit,
     ) {
-        repository.downloadPlaylist(playlistRequest, executor, onDownloadInfo)
+        val playlist =
+            repository.getStoredYoutubePlaylistByName(playlistName = playlistRequest.playlistInfo!!.name)
+        val isAllVideosDownloaded = playlist?.videos?.all { it.isDownloaded }
+        if (isAllVideosDownloaded == false)
+            repository.downloadPlaylist(playlistRequest, executor, onDownloadInfo)
+        else
+            onDownloadInfo(DataState.success(DownloadInfo(100f,
+                0,
+                playlist!!.videos.lastIndex,
+                true,
+                null)))
     }
 
 }
