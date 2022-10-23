8b1895adfb24a6fc424bfca6713d1f6af3002c3b
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-10-24 00:07:47 +0200

    add: QUALITY_TRANSLATED_CONSTANTS and quality extractor from string

--- app/src/main/java/com/example/vidme/domain/pojo/request/Request.kt
+++ app/src/main/java/com/example/vidme/domain/pojo/request/Request.kt
@@ -21,20 +21,44 @@ abstract class Request(
         const val TYPE_VIDEO = "video"
         private const val TYPE_VIDEO_AR = "فيديو"
         const val QUALITY_BEST = "best"
+        private const val QUALITY_BEST_AR: String = "الأفضل"
         const val QUALITY_WORST = "worst"
+        private const val QUALITY_WORST_AR = "الاسوأ"
 
         fun typeFromString(string: String): String {
-            return if (string in audioTypes) TYPE_AUDIO else TYPE_VIDEO
+            return when (string) {
+                in audioTypes -> TYPE_AUDIO
+                in videoTypes -> TYPE_VIDEO
+                else -> throw IllegalStateException("$string is not a valid type, type must be one of ${audioTypes + videoTypes}")
+            }
         }
 
-        private val audioTypes = listOf(
+        fun qualityFromString(string: String): String {
+            return when (string) {
+                in qualityBest -> QUALITY_BEST
+                in qualityWorst -> QUALITY_WORST
+                else -> throw IllegalStateException("$string is not a valid quality, quality must be one of ${qualityBest + qualityWorst}")
+            }
+        }
+
+        private val audioTypes = arrayOf(
             TYPE_AUDIO,
             TYPE_AUDIO_AR,
         )
 
-        private val videoTypes = listOf(
+        private val videoTypes = arrayOf(
             TYPE_VIDEO,
             TYPE_VIDEO_AR,
+        )
+
+        private val qualityBest = arrayOf(
+            QUALITY_BEST,
+            QUALITY_BEST_AR
+        )
+
+        private val qualityWorst = arrayOf(
+            QUALITY_WORST,
+            QUALITY_WORST_AR
         )
 
     }
