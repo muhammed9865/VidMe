3149b900d433c51f176bdd7b9c6d90a79bfd49aa
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-10-23 23:59:39 +0200

    add: TYPES_TRANSLATED_CONSTANTS and type extractor from string

--- app/src/main/java/com/example/vidme/domain/pojo/request/Request.kt
+++ app/src/main/java/com/example/vidme/domain/pojo/request/Request.kt
@@ -12,19 +12,30 @@ abstract class Request(
         }
     }
 
-    @Suppress("UNCHECKED_CAST")
-    fun <T : Request> update(func: (request: T) -> T): T {
-        return func(this as T)
-    }
-
 
     fun isAudio() = type == TYPE_AUDIO
 
     companion object {
         const val TYPE_AUDIO = "audio"
+        private const val TYPE_AUDIO_AR = "صوت"
         const val TYPE_VIDEO = "video"
+        private const val TYPE_VIDEO_AR = "فيديو"
         const val QUALITY_BEST = "best"
         const val QUALITY_WORST = "worst"
+
+        fun typeFromString(string: String): String {
+            return if (string in audioTypes) TYPE_AUDIO else TYPE_VIDEO
+        }
+
+        private val audioTypes = listOf(
+            TYPE_AUDIO,
+            TYPE_AUDIO_AR,
+        )
+
+        private val videoTypes = listOf(
+            TYPE_VIDEO,
+            TYPE_VIDEO_AR,
+        )
 
     }
 }
